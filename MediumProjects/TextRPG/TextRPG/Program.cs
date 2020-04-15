d204259f6a02bff8cc8afb9a32f4ec88b7cdb127
Kai Devrim <kkgithub@koldkai.com>
2020-04-14 21:30:50 -0700

    Created the Visual Studio 2019 Projects

---
+++ MediumProjects/TextRPG/TextRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
