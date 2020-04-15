d97fce6e15bc9ec3ef1deca34d4b7ddffd1c35f0
Kai Devrim <kkgithub@koldkai.com>
2020-04-14 21:22:40 -0700

    Started making them projects

---
+++ EasyProjects/RomanNumeral2Integer/RomanNumeral2Integer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumeral2Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
