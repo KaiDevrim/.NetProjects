4e1261d93e701138e31a24f9b1aec3f9cca75616
Kai Devrim <kkgithub@koldkai.com>
2020-04-29 13:34:38 -0700

    ye

--- HardProjects/CurrencyConverterAPI/CurrencyConverterAPI/Program.cs
+++ HardProjects/CurrencyConverterAPI/CurrencyConverterAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.IO;
 
 namespace CurrencyConverterAPI
 {
@@ -6,7 +8,13 @@ namespace CurrencyConverterAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string contents = File.ReadAllText(@"C:\Users\Devrim\Documents\GitHub\.NetProjects\HardProjects\CurrencyConverterAPI\CurrencyConverterAPI\token.txt");
+            var client = new RestClient("https://currency-converter5.p.rapidapi.com/currency/convert?&amp;from=usd&amp;to=try&amp;amount=10&amp;format=json");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "currency-converter5.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", contents);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response);
         }
     }
 }
